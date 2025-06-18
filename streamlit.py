ea6a61927bdd4df80345eefa88f16d17a8144735
Alexander Steinmaurer <a@steinmaurer.cc>
2025-06-18 13:35:31 +0200

    replace LLM

--- streamlit.py
+++ streamlit.py
@@ -37,7 +37,7 @@ with st.sidebar.form("config_form"):
         "gpt-4",
         "gpt-4o",
         "deepseek/deepseek-r1-0528-qwen3-8b:free",
-        "meta-llama/llama-3.2-1b-instruct:free", 
+        "google/gemma-3-1b-it:free", 
         "mistralai/mistral-7b-instruct:free"
     ])
     temperature = st.slider("Temperature", 0.0, 1.0, 0.7, 0.1)
