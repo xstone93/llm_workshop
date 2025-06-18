aa3ea83cab1f64c8c7ad6dff48d71ac282ff36b8
Alexander Steinmaurer <a@steinmaurer.cc>
2025-06-18 13:17:52 +0200

    Adding model

--- streamlit.py
+++ streamlit.py
@@ -37,7 +37,7 @@ with st.sidebar.form("config_form"):
         "gpt-4",
         "gpt-4o",
         "deepseek/deepseek-r1-0528-qwen3-8b:free",
-        "qwen/qwen3-0.6b-04-28:free", 
+        "opengvlab/internvl3-2b:free", 
         "mistralai/mistral-7b-instruct:free"
     ])
     temperature = st.slider("Temperature", 0.0, 1.0, 0.7, 0.1)
